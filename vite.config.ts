8996f8c6c1d2b2311ddefa9950f3f0281fac153c
Javier Sanz Sáez <javiersse@gmail.com>
2025-10-31 11:37:06 +0100

    Set Vite port

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    host: true, // This allows access from network interfaces
+    open: true  // This will automatically open the browser
+  }
 })
